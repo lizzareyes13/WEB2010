db08e33e70ffb5c3f5f436cee5bea15bef27693d
Lizza Reyes <lizzareyes13@gmail.com>
2016-07-28 17:34:24 -0400

    not finished, ran out of battery: will continue after charging

---
+++ main-answers.js
@@ -0,0 +1,31 @@
+1.  class Task {
+  constructor(ownr,desc) {
+    this.ownr = owner;
+    this.desc = description;
+
+  }
+  if Task = this.ownr + this.desc{
+    alert ("Let's get started!")
+  }
+  else {
+    alert("please make sure to fill out all fields...")
+  }
+  var setTime = 
+}
+
+
+2.
+
+3.
+
+4.
+
+5.
+
+6.
+
+7.
+
+8.
+
+9.
